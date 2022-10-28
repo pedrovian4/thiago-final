58b4244ab3b7f6f0cc9d3619d1ee756aed84a70f
Breno <bcaua205@gmail.com>
2022-10-27 22:23:53 -0300

    adicionando arquivos vs

--- table/main.sql
+++ table/main.sql
@@ -1,9 +1,12 @@
+create database pinterest;
+use pinterest;
+
 create table user(
 	id int not null auto_increment,
 	password varchar(150) not null,
 	email varchar(150) not null,
-	created_at datetime not null,
-	updated_at datetime not null,
+	create_time datetime not null,
+	update_time datetime not null,
 	constraint PK_User primary key (id)
 );
 
