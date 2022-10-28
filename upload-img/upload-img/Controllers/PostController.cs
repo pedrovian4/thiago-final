565800633b8e07ef7466981f64610a1364b4a0ab
Breno <bcaua205@gmail.com>
2022-10-27 22:13:41 -0300

    adicionando api

---
+++ upload-img/upload-img/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace upload_img.Controllers
+{
+    [Route("api/{controller}")]
+    [ApiController]
+    public class PostController : ControllerBase 
+    {
+        [HttpPost]
+        public IActionResult Post([FromForm] PostDTO post)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(post);
+        }
+    }
+}
---
+++ upload-img/upload-img/Tools/AllowedExtensionsAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace upload_img.Tools
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public new string ErrorMessage { get; set; } = "The file \"{}\" is not allowed!";
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            if (value is IEnumerable<IFormFile>)
+            {
+                var files = value as IEnumerable<IFormFile>;
+                List<string> errors = new List<string>();
+                foreach (var file in files)
+                {
+                    bool isFileValid = ValidateFileExtension(file);
+                    if (!isFileValid)
+                    {
+                        errors.Add(file.FileName);
+                    }
+                }
+
+                if (errors.Count > 0)
+                    return new ValidationResult(ErrorMessage.Replace("{}", string.Join(", ", errors)));
+
+            }
+            else if (value is IFormFile)
+            {
+                var file = value as IFormFile;
+                bool isFileValid = ValidateFileExtension(file);
+                if (!isFileValid)
+                    return new ValidationResult(ErrorMessage.Replace("{}", file.FileName));
+            }
+
+            return ValidationResult.Success;
+        }
+
+        protected bool ValidateFileExtension(IFormFile file)
+        {
+            var extension = Path.GetExtension(file.FileName);
+            if (_extensions.Contains(extension.ToLower()))
+                return true;
+            else
+                return false;
+        }
+    }
+}
