2d8aec43868565740af5c40963c89ea037130dd8
Pedro <pedrovianaasking@gmail.com>
2022-11-03 00:59:38 -0300

    Ajustando  para da curtida em posts

--- recomendations/src/index.js
+++ recomendations/src/index.js
@@ -1,31 +1,34 @@
 
 // Não usei padrão de arquitetura porque isso aqui é simples logo é desnecessário
 const express = require('express');
-const app = express();
 const mysql = require("mysql"); 
-const connection = mysql.createConnection({
+const  multer = require("multer");
+const upload = multer();
+
+const connection = mysql.createPool({
+    connectionLimit:10,
     host: 'localhost', 
     user:'root', 
     password:'root', 
     database:'pinterest'
 });
 
-
+const app = express();
 app.use(express.json());
 
+
+
 async function getUser (res,id){ 
-    connection.connect();
     connection.query(`select * from Users where id =${id}`, (error, results, fields)=>{
         if (error) throw error;
 
         res.send(results);
     });
 
-    connection.end();
+
 }
 
 async function getUsers (res){ 
-    connection.connect();
     connection.query("select * from Users", (error, results, fields)=>{
         if (error) throw error;
 
@@ -33,35 +36,29 @@ async function getUsers (res){
         return results;
     });
 
-    connection.end();
 }
 
 async function getPosts(res){
-    connection.connect();
     const sql = "select Posts.title,Posts.img_url, Tags.tag_name, Posts.id as postid, Tags.id as tagid from Post_tags left join Posts on Post_tags.post_id = Posts.id left join Tags on Post_tags.tag_id = Tags.id"
     connection.query(sql, (error, result, fields) =>{
         if (error) throw error;
         res.send(result);
     } )
-    connection.end();
 }
 
 async function getUserLiked(res){
-    connection.connect();
     const sql = "select u.id as userId, u.first_name as userName, p.title as postTitle, p.id as PostId from Post_likes pl left join Posts p on pl.post_id = p.id left join Users u on pl.user_liked_id = u.id";
 
     connection.query(sql, (error, result, fields) =>{
         if (error) throw error;
         res.send(result);
     } );
-    connection.end();
 }
 
 
 
 async function getUserRecomendation(res,id){
 
-    connection.connect();
     const sql = `select * from recomendations r left join Posts p on r.post_id = p.id left join Users u on r.user_id = u.id  where r.user_id =  ${id}`;
 
     connection.query(sql, async (error, results, fields) =>{
@@ -69,11 +66,43 @@ async function getUserRecomendation(res,id){
 
         results>[] ? res.send(results) : res.send("Usuário não curtiu nenhum post para ter recomendações!");
     } );
-
-    connection.end();
-
 }
 
+
+async function likeUser(userId, postId, res){
+
+    const insertsql  =`insert into Post_Likes (post_id, user_liked_id) values  (${userId},${postId})`;
+
+    const check = async ()=>{
+        const sqlCheckIfPostExists = `select 1 from Posts where id = ${postId}`; 
+        const sqlCheckIfUserExists = `select 1 from Users where id = ${userId}`;
+        const checkpost = connection.query(sqlCheckIfPostExists, async(error, results, fields)=>{
+            if (error) throw error;
+            if (results.length >0) return true;
+            
+            return false;  
+              });
+        const checkuser  = connection.query(sqlCheckIfUserExists, async(error, results, fields)=>{
+            if (error) throw error;
+            if (results.length >0) return true;
+            
+            return false;
+        });
+
+        return checkuser  && checkpost;
+    }
+
+    if(check){
+        connection.query(insertsql, async (error, results, fields)=>{
+            if (error) throw error;
+
+            res.send("User inserted");
+            return;
+        });
+    
+    res.send("Post Não existe ou Usuario ou Post ja foi curtido pelo usuario");
+    }
+}
 
 
 
@@ -97,6 +126,17 @@ app.get('/likes', async(req, res)=>{
 
 app.get('/recomendations/:userID', async( req,res) =>{
     getUserRecomendation(res,req.params.userID);
+});
+
+app.post('/likeUserPost',upload.none() ,async (req, res)=>{
+
+    const userId = req.body.userId; 
+    const postId = req.body.postId;
+
+    // console.log(req.body);
+    // if(userId && postId) likeUser(userId, postId, res);
+
+    res.send('Valores nulos');
 });
 
 
